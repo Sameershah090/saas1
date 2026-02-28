e6523cde1488fb83c268ee27c74ab49e55c749ce
samir bagwan <sameershahh090@gmail.com>
2026-03-01 01:13:27 +0530

    Delete falsegram directory

--- falsegram/src/database/index.js
+++
@@ -1,8 +0,0 @@
-const { getDatabase, closeDatabase } = require('./connection');
-const Contact = require('./models/Contact');
-const MessageMap = require('./models/MessageMap');
-const CallRecord = require('./models/CallRecord');
-const ScheduledMessage = require('./models/ScheduledMessage');
-const ReactionMap = require('./models/ReactionMap');
-
-module.exports = { getDatabase, closeDatabase, Contact, MessageMap, CallRecord, ScheduledMessage, ReactionMap };
